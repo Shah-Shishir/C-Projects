71a6045d15cb2e567f4e2242bedb8093b36b0aad
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-02-15 21:32:58 +0600

    Add files via upload

---
+++ RectangleApplication.cs
@@ -0,0 +1,41 @@
+﻿Ideone link : https://ideone.com/hk1ePB
+
+using System;
+
+namespace RectangleApplication
+{
+    class MyRectangle
+    {
+        double length;
+        double width;
+
+        public void Acceptdetails ()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+
+        public double GetArea ()
+        {
+            return length * width;
+        }
+
+        public void Print ()
+        {
+            Console.WriteLine("Length : {0}",length);
+            Console.WriteLine("Width : {0}", width);
+            Console.WriteLine("Area : {0}", GetArea());
+        }
+    }
+
+    class ExecuteRectangle
+    {
+        static void Main (string[] args)
+        {
+            MyRectangle r = new MyRectangle();
+
+            r.Acceptdetails ();
+            r.Print ();
+        }
+    }
+}
