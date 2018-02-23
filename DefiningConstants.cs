cfdf4f05961b05e2f081d73c2181daf30ea2c14e
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-02-23 21:56:16 +0600

    Add files via upload

---
+++ DefiningConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DefiningConstants
+{
+    public class Const
+    {
+        public static void Main()
+        {
+            const double pi = 3.14159;
+            double r, A;
+
+            Console.Write("Enter the radius of the circle : ");
+            r = Convert.ToDouble(Console.ReadLine());
+            A = pi * r * r;
+
+            Console.WriteLine("Radius : {0}, Area = {1}", r, A);
+        }
+    }
+}
