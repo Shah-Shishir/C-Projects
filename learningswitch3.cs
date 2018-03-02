a7c12e6d9d7a6d9d2abc35cd70eb6ebb45d4cfd7
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-03-02 21:59:08 +0600

    Add files via upload

---
+++ learningswitch3.cs
@@ -0,0 +1,45 @@
+﻿/*** Ideone Link : learningswitch3 ***/
+
+using System;
+
+namespace learningswitch3
+{
+    class Switch
+    {
+        public static void Main(string[] args)
+        {
+            int a = 100, b = 200;
+
+            switch (a)
+            {
+                case 50:
+                    {
+                        Console.WriteLine("a = 50");
+
+                        switch (b)
+                        {
+                            case 200:
+                                Console.WriteLine("b = 200");
+                                break;
+                        }
+                    }
+                    break;
+
+                case 100:
+                    {
+                        Console.WriteLine("a = 100");
+
+                        switch (b)
+                        {
+                            case 200:
+                                Console.WriteLine("b = 200");
+                                break;
+                        }
+                    }
+                    break;
+            }
+
+            Console.WriteLine("Actual value of a is {0}\nActual value of b is {1}", a, b);
+        }
+    }
+}
