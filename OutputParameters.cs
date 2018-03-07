51bf121e135da7aa15a0b84b4ba9e8193082a11d
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-03-07 23:59:14 +0600

    Add files via upload

---
+++ OutputParameters.cs
@@ -0,0 +1,38 @@
+﻿/*** 
+
+	The difference between Reference Parameters and Output Parameters in my perspective is -> 
+For Reference Parameters, the value needs to be assigned into the variable, but For Output Parameters, 
+it doesn't.
+
+    Ideone Link : https://ideone.com/5nZ8fa
+
+***/
+
+using System;
+
+namespace OutputParameters
+{
+    class Secondary
+    {
+        public void Scanner(out int a, out int b)
+        {
+            Console.Write("Enter the value of a : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value of b : ");
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    class Primary
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Secondary r = new Secondary();
+            r.Scanner(out a, out b);
+
+            Console.WriteLine("Value of a : {0}, Value of b : {1}", a, b);
+        }
+    }
+}
