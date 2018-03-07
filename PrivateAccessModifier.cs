57a8c439fda4948403ebe5153af40c8aaa534d7f
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-03-07 21:41:38 +0600

    Add files via upload

---
+++ PrivateAccessModifier.cs
@@ -0,0 +1,30 @@
+﻿/*** Ideone Link : https://ideone.com/l5mts7 ***/
+
+using System;
+
+namespace PrivateAccessModifier
+{
+    class Area
+    {
+        double b = 4.5, h = 3.5;
+
+        double GetArea()
+        {
+            return 0.5 * b * h;
+        }
+
+        public void PrintArea()
+        {
+            Console.WriteLine("Base : {0}, Height : {1}, Area : {2}", b, h, GetArea());
+        }
+    }
+
+    class Perform
+    {
+        static void Main(string[] args)
+        {
+            Area r = new Area();
+            r.PrintArea();
+        }
+    }
+}
