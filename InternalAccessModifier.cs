92371f8c685f7b3fc2475fb5cd5cce92ecaddeda
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-03-07 21:42:11 +0600

    Add files via upload

---
+++ InternalAccessModifier.cs
@@ -0,0 +1,34 @@
+﻿/*** Ideone Link : https://ideone.com/8dnwF4 ***/
+
+using System;
+
+namespace InternalAccessModifier
+{
+    class Area
+    {
+        internal double b, h;
+
+        double GetArea()
+        {
+            return 0.5 * b * h;
+        }
+
+        public void PrintArea()
+        {
+            Console.WriteLine("Base : {0}, Height : {1}, Area : {2}", b, h, GetArea());
+        }
+    }
+
+    class Perform
+    {
+        static void Main(string[] args)
+        {
+            Area r = new Area();
+
+            r.b = 4.5;
+            r.h = 3.5;
+
+            r.PrintArea();
+        }
+    }
+}
