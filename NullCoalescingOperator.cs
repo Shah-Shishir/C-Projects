a7b2ebde633e164073e7fcb3e878e44218e7ba03
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-03-08 22:52:04 +0600

    Add files via upload

---
+++ NullCoalescingOperator.cs
@@ -0,0 +1,22 @@
+﻿/*** Ideone Link : https://ideone.com/Yw3xpR ***/
+
+using System;
+
+namespace NullCoalescingOperator
+{
+    class Actual
+    {
+        static void Main(string[] args)
+        {
+            double? a = null;
+            double? b = 3.14159;
+            double? c;
+            c = a ?? 5.34;
+            Console.WriteLine("Nullable value, c = {0}", c);
+            c = a ?? b;
+            Console.WriteLine("Nullable value, c = {0}", c);
+            c = b ?? 5.34;
+            Console.WriteLine("Nullable value, c = {0}", c);
+        }
+    }
+}
