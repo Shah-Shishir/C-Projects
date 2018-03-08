27bbe089ebb1b64970e014a391445feada51ed88
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-03-08 22:51:15 +0600

    Add files via upload

---
+++ NullablesIntro.cs
@@ -0,0 +1,20 @@
+﻿/*** Ideone Link : https://ideone.com/1jsOfw ***/
+
+using System;
+
+namespace NullablesIntro
+{
+    class Actual
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+            int? b = 12;
+            int? c = new int?();
+            double? d = new double?();
+            bool? e = new bool?();
+
+            Console.WriteLine("Nullable values\na = {0}, b = {1}, c = {2}, d = {3}, e = {4}", a, b, c, d, e);
+        }
+    }
+}
